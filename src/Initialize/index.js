587d67d9042e3aa5b908095ee82d4040c27c7923
pmpurcell93 <maddenpurcell@gmail.com>
2021-12-02 20:16:02 -0600

    Set up user auth

--- src/Initialize/index.js
+++ src/Initialize/index.js
@@ -1,7 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import { signInUser, signOutUser } from '../api/auth';
 
 function Initialize() {
   const [domWriting, setDomWriting] = useState('Nothing Here!');
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((authed) => {
+      if (authed) {
+        const userInfoObj = {
+          fullName: authed.displayName,
+          uid: authed.uid,
+          isAdmin: process.env.REACT_APP_ADMIN_UID === authed.uid,
+        };
+        setUser(userInfoObj);
+        console.warn(userInfoObj);
+      } else if (user || user === null) {
+        setUser(false);
+      }
+    });
+  }, []);
 
   const handleClick = (e) => {
     console.warn(`You clicked ${e.target.id}`);
@@ -30,6 +50,25 @@ function Initialize() {
         >
           I am THAT button
         </button>
+        {user ? (
+          <button
+            type="button"
+            id="that-button"
+            className="btn btn-primary mt-3"
+            onClick={signOutUser}
+          >
+            Sign Out
+          </button>
+        ) : (
+          <button
+            type="button"
+            id="that-button"
+            className="btn btn-primary mt-3"
+            onClick={signInUser}
+          >
+            Sign In
+          </button>
+        )}
       </div>
       <h3>{domWriting}</h3>
     </div>
