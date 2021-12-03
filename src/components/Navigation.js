f56833c27593bda4cc181e32c6fd215d41ada623
pmpurcell93 <maddenpurcell@gmail.com>
2021-12-02 20:51:56 -0600

    Added navbar component

---
+++ src/components/Navigation.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import {
+  ProSidebar, Menu, MenuItem, SidebarFooter,
+} from 'react-pro-sidebar';
+import 'react-pro-sidebar/dist/css/styles.css';
+// import { Link } from 'react-router-dom';
+import { PropTypes } from 'prop-types';
+import { signInUser, signOutUser } from '../api/auth';
+
+export default function Navigation({ user }) {
+  return (
+    <div>
+      <ProSidebar>
+        <Menu iconShape="square">
+          <MenuItem>
+            Home
+            {/* <Link to="/" /> */}
+          </MenuItem>
+          <MenuItem>
+            Chords
+            {/* <Link to="/chords" /> */}
+          </MenuItem>
+          <MenuItem>
+            Charts
+            {/* <Link to="/charts" /> */}
+          </MenuItem>
+          <MenuItem>
+            Tuner
+            {/* <Link to="/tuner" /> */}
+          </MenuItem>
+          {user ? (
+            <MenuItem
+              type="button"
+              className="btn btn-secondary"
+              onClick={signOutUser}
+            >
+              Sign Out
+            </MenuItem>
+          ) : (
+            <MenuItem
+              type="button"
+              className="btn btn-secondary"
+              onClick={signInUser}
+            >
+              Sign In
+            </MenuItem>
+          )}
+        </Menu>
+        <SidebarFooter>© Madden Purcell, 2021</SidebarFooter>
+      </ProSidebar>
+      ;
+    </div>
+  );
+}
+
+Navigation.propTypes = {
+  user: PropTypes.shape({
+    isAdmin: PropTypes.bool,
+  }),
+};
+
+Navigation.defaultProps = {
+  user: {},
+};
