dd65eede8f875bd830c29a861f2f85e61e65e6d1
pmpurcell93 <maddenpurcell@gmail.com>
2021-12-13 19:45:29 -0600

    File upload on update chart

--- src/views/Tuner.js
+++ src/views/Tuner.js
@@ -4,6 +4,19 @@ export default function Tuner() {
   return (
     <div>
       <h1>Tuner</h1>
+      <img
+        src="https://media.istockphoto.com/photos/playing-ukulele-picture-id477710820?k=20&m=477710820&s=612x612&w=0&h=f27eIuyv7peSPPh_wivykGeieJYC3gI7wuRWozB1K1g="
+        alt="Ukulele"
+      />
+      {/* <div id="buttonDiv">
+        <button type="button">
+          <audio
+            src="https://www.soundsnap.com/node/2602">
+            <track default kind="descriptions" />
+          </audio>
+          G
+        </button>
+      </div> */}
     </div>
   );
 }
