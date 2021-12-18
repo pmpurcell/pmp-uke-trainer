5c388e89c06e74ae6ee318e4c2a59035e124d966
pmpurcell93 <maddenpurcell@gmail.com>
2021-12-17 19:59:29 -0600

    Navbar responsiveness

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,6 +5,10 @@ import {
 import 'react-pro-sidebar/dist/css/styles.css';
 import { Link } from 'react-router-dom';
 import { PropTypes } from 'prop-types';
+import {
+  BsFillArrowLeftSquareFill,
+  BsFillArrowRightSquareFill,
+} from 'react-icons/bs';
 import { signInUser, signOutUser } from '../api/auth';
 
 export default function Navigation({ user }) {
@@ -13,48 +17,60 @@ export default function Navigation({ user }) {
     <div id="navbar">
       <ProSidebar collapsed={collapsed}>
         <Menu iconShape="square">
-          <MenuItem
-            type="button"
-            className="btn btn-secondary"
-            onClick={() => setCollapsed(!collapsed)}
-          >
-            Collapse
-          </MenuItem>
-          <MenuItem>
-            Home
-            <Link to="/" />
-          </MenuItem>
-          <MenuItem>
-            Chords
-            <Link to="/chords" />
-          </MenuItem>
-          <MenuItem>
-            Charts
-            <Link to="/charts" />
-          </MenuItem>
-          <MenuItem>
-            Tuner
-            <Link to="/tuner" />
-          </MenuItem>
-          {user ? (
-            <>
-              <MenuItem>User: {user.fullName}</MenuItem>
-              <MenuItem
-                type="button"
-                className="btn btn-secondary"
-                onClick={signOutUser}
-              >
-                Sign Out
-              </MenuItem>
-            </>
-          ) : (
+          {collapsed ? (
             <MenuItem
               type="button"
-              className="btn btn-secondary"
-              onClick={signInUser}
+              // className="btn btn-secondary"
+              onClick={() => setCollapsed(!collapsed)}
             >
-              Sign In
+              <BsFillArrowLeftSquareFill />
             </MenuItem>
+          ) : (
+            <>
+              <MenuItem
+                type="button"
+                // className="btn btn-secondary"
+                onClick={() => setCollapsed(!collapsed)}
+              >
+                <BsFillArrowRightSquareFill />
+              </MenuItem>
+              <MenuItem>
+                Home
+                <Link to="/" />
+              </MenuItem>
+              <MenuItem>
+                Chords
+                <Link to="/chords" />
+              </MenuItem>
+              <MenuItem>
+                Charts
+                <Link to="/charts" />
+              </MenuItem>
+              <MenuItem>
+                Tuner
+                <Link to="/tuner" />
+              </MenuItem>
+              {user ? (
+                <>
+                  <MenuItem>Welcome, {user.fullName}!</MenuItem>
+                  <MenuItem
+                    type="button"
+                    className="btn btn-secondary"
+                    onClick={signOutUser}
+                  >
+                    Sign Out
+                  </MenuItem>
+                </>
+              ) : (
+                <MenuItem
+                  type="button"
+                  className="btn btn-secondary"
+                  onClick={signInUser}
+                >
+                  Sign In
+                </MenuItem>
+              )}
+            </>
           )}
         </Menu>
         <SidebarFooter>© Madden Purcell, 2021</SidebarFooter>
